fd49c00194fbfd5a2e2d558dd41052d0cc4c72e7
mstermaaten <mstermaaten@gmail.com>
2019-08-29 21:12:44 +0200

    quill working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Quill from "quill2-dev";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    var options = {
+      theme: "snow",
+      modules: {
+        table: true,
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline"],
+          ["image", "code-block"]
+        ]
+      }
+    };
+
+    var editor = new Quill("#editor-container", options);
+    document
+      .querySelector("#insert-table")
+      .addEventListener("click", function() {
+        debugger;
+        const table = editor.getModule("table");
+        table.insertTable(2, 2);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div id="editor-container"></div>
+        <button id="insert-table">Insert</button>
+      </div>
+    );
+  }
 }
 
 export default App;
